a93660cfe2716d546d6a7c3a2b5cb21370a60350
Watashis <Vlad73RU86@yandex.ru>
2020-07-20 20:02:54 +0400

    Added search

---
+++ RList/RMain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RList
+{
+    class RMain
+    {
+        public string getHttp(string url)
+        {
+            System.Net.ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+            var request = (System.Net.HttpWebRequest)System.Net.WebRequest.Create(url);
+            var response = (System.Net.HttpWebResponse)request.GetResponse();
+            var responseString = new System.IO.StreamReader(response.GetResponseStream()).ReadToEnd();
+            response.Close();
+            return responseString;
+        }
+    }
+}
